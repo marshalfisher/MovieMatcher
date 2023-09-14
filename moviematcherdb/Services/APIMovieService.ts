e3f1b4c8a643af4d079962f93bba7b27d382de23
Marshal Fisher <marshal_fisher@yahoo.com>
2023-09-14 18:28:40 -0500

    Update APIMovieService.ts

--- moviematcherdb/Services/APIMovieService.ts
+++ moviematcherdb/Services/APIMovieService.ts
@@ -20,7 +20,7 @@ export const APIMovieService = {
 
   getPopularMovies: async(): Promise<IResults> => {
     try {
-      const popularMovies = await axios.get('https://api.themoviedb.org/3/discover/movie/?api_key=66be68e2d9a8be7fee88a803b45d654b&watch_region=US&with_watch_providers=10')
+      const popularMovies = await axios.get('https://api.themoviedb.org/3/discover/movie/?api_key=*&watch_region=US&with_watch_providers=10')
       return  popularMovies.data;//.json();
     } catch (e) {
       console.log(e);
@@ -30,7 +30,7 @@ export const APIMovieService = {
 
   getUpcomingMovies: async(): Promise<IResults> => {
     try {
-      const latestMovies = await axios.get('https://api.themoviedb.org/3/movie/now_playing?api_key=66be68e2d9a8be7fee88a803b45d654b&language=en-US&page=1')
+      const latestMovies = await axios.get('https://api.themoviedb.org/3/movie/now_playing?api_key=*&language=en-US&page=1')
       return latestMovies.data;//.json();
     } catch (e) {
       console.log(e);
@@ -39,14 +39,14 @@ export const APIMovieService = {
   },
 
   getFilteredMoviesQuery: async(params: string)  => {
-    const movies = await axios.get('https://api.themoviedb.org/3/discover/movie?api_key=48343d08ec9aa87fbbfecd658bbc7ba9&language=en-US&include_adult=true&include_video=false&watch_region=US' + params)
+    const movies = await axios.get('https://api.themoviedb.org/3/discover/movie?api_key=*&language=en-US&include_adult=true&include_video=false&watch_region=US' + params)
     return movies.data;
   },
 
   getCastID: async(castString: string) => {
     try{
     const cast = castString.trim().replace(' ', '%20');
-    const castIDArr = await axios.get(`https://api.themoviedb.org/3/search/person?api_key=48343d08ec9aa87fbbfecd658bbc7ba9&language=en-US&query=${cast}&page=1&include_adult=false`)
+    const castIDArr = await axios.get(`https://api.themoviedb.org/3/search/person?api_key=*&language=en-US&query=${cast}&page=1&include_adult=false`)
     if(castIDArr.data.results.length === 0){
       return '';
     }
@@ -60,7 +60,7 @@ export const APIMovieService = {
 
   getMoviesBase: async() => {
     try{
-    const movies = await axios.get('https://api.themoviedb.org/3/discover/movie?api_key=48343d08ec9aa87fbbfecd658bbc7ba9&language=en-US&sort_by=popularity.desc&include_adult=true&include_video=false')
+    const movies = await axios.get('https://api.themoviedb.org/3/discover/movie?api_key=*&language=en-US&sort_by=popularity.desc&include_adult=true&include_video=false')
     return movies.data;
     } catch(err){
       console.log(err);
@@ -69,7 +69,7 @@ export const APIMovieService = {
 
   getIndividualMovie: async(id:string | number): Promise<IMovieDetails>  => {
     try {
-      const movie  = await axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=66be68e2d9a8be7fee88a803b45d654b&language=en`);
+      const movie  = await axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=*&language=en`);
       return await movie//.json()
     }catch(e) {
       console.log(e);
@@ -78,7 +78,7 @@ export const APIMovieService = {
   },
   getActorListQuery: async(id:number): Promise<IActorList> => {
     try {
-        const actorList = await axios.get(`https://api.themoviedb.org/3/movie/${id}/credits?api_key=66be68e2d9a8be7fee88a803b45d654b`)
+        const actorList = await axios.get(`https://api.themoviedb.org/3/movie/${id}/credits?api_key=*`)
         return actorList.data//.json()
     } catch(err) {
       console.log(err)
@@ -87,7 +87,7 @@ export const APIMovieService = {
   },
   getStreamProvidersQuery: async(id:number): Promise<any> => {
     try {
-      const streamProvider = await axios.get(`https://api.themoviedb.org/3/movie/${id}/watch/providers?api_key=66be68e2d9a8be7fee88a803b45d654b`)
+      const streamProvider = await axios.get(`https://api.themoviedb.org/3/movie/${id}/watch/providers?api_key=*`)
       const data = streamProvider.data//.json();
       return data.results;
     }catch(err) {
@@ -97,7 +97,7 @@ export const APIMovieService = {
   },
   getSimilarMoviesQuery: async(id:number): Promise<IResults> => {
     try {
-      const similarMovies = await axios.get(`https://api.themoviedb.org/3/movie/${id}/similar?api_key=66be68e2d9a8be7fee88a803b45d654b&with_watch_providers=10&watch_region=US`)
+      const similarMovies = await axios.get(`https://api.themoviedb.org/3/movie/${id}/similar?api_key=*&with_watch_providers=10&watch_region=US`)
       return similarMovies.data//.json();
     } catch (e) {
       console.log(e);
@@ -106,7 +106,7 @@ export const APIMovieService = {
   },
   getActorDetailsQuery: async(actorId:number): Promise<ActorDetailsInterface> => {
     try {
-      const actorDetails = await axios.get(`https://api.themoviedb.org/3/person/${actorId}?api_key=66be68e2d9a8be7fee88a803b45d654b`)
+      const actorDetails = await axios.get(`https://api.themoviedb.org/3/person/${actorId}?api_key=*`)
       return actorDetails.data//.json();
 
     }catch(e) {
@@ -116,7 +116,7 @@ export const APIMovieService = {
   },
   getCombinedCreditsQuery: async(actorId:number): Promise<IMovie[]> => {
     try {
-      const similarMovies = await axios.get(`https://api.themoviedb.org/3/person/${actorId}/combined_credits?api_key=66be68e2d9a8be7fee88a803b45d654b`)
+      const similarMovies = await axios.get(`https://api.themoviedb.org/3/person/${actorId}/combined_credits?api_key=*`)
       let data = await similarMovies//.json();
       return data.cast
     }catch(err) {
@@ -126,7 +126,7 @@ export const APIMovieService = {
   },
   getSpecificMovieQuery: async(id: number): Promise<IMovieDetails> => {
     try {
-      const movie  = await axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=66be68e2d9a8be7fee88a803b45d654b&language=en`);
+      const movie  = await axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=*&language=en`);
       return movie.data;
     } catch(err) {
      console.log(err);
